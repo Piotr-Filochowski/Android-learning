701a867e29cde3f271af41ba631842eddea7225a
Piotr Filochowski <piotr.filochowski01@gmail.com>
2020-12-06 13:27:17 +0100

    Switch with buttons color and shared preferences

--- app/src/main/java/com/filochowski/smb_cw1/activity/MainActivity.kt
+++ app/src/main/java/com/filochowski/smb_cw1/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.filochowski.smb_cw1.activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -34,11 +35,42 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent1)
         }
 
+
+
+        binding.changeColour.setOnCheckedChangeListener { _ , isChecked ->
+            val message = if (isChecked) "Blue" else "Red"
+            if (isChecked) {
+                binding.button.setBackgroundColor(Color.BLUE)
+                binding.buttonNext .setBackgroundColor(Color.BLUE)
+                binding.changeColour.setTextColor(Color.BLUE)
+            } else {
+                binding.button.setBackgroundColor(Color.RED)
+                binding.buttonNext.setBackgroundColor(Color.RED)
+                binding.changeColour.setTextColor(Color.RED)
+            }
+
+
+            Toast.makeText(this@MainActivity, message,
+                Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
         binding.someText.text = sharedPreferences.getString("tv_text", getString(R.string.nothing_was_saved_before))
+        var color = sharedPreferences.getBoolean("color", true)
+        if(color){
+            binding.button.setBackgroundColor(Color.BLUE)
+            binding.buttonNext .setBackgroundColor(Color.BLUE)
+            binding.changeColour.setTextColor(Color.BLUE)
+            binding.changeColour.setChecked(color)
+        } else {
+            binding.button.setBackgroundColor(Color.RED)
+            binding.buttonNext.setBackgroundColor(Color.RED)
+            binding.changeColour.setTextColor(Color.RED)
+            binding.changeColour.setChecked(color)
+        }
     }
 
 
@@ -46,6 +78,7 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         val editor = sharedPreferences.edit()
         editor.putString("tv_text", binding.someText.text.toString())
+        editor.putBoolean("color", binding.changeColour.isChecked)
         editor.apply()
     }
 }
